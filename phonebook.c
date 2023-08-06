95b1421320c0934128ea1d589d9136a45c3e70b1
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-06 02:03:07 +0000

    Sun, Aug 6, 2023, 11:03 AM +09:00

--- phonebook.c
+++ phonebook.c
@@ -1,11 +1,39 @@
 #include <stdio.h>
 #include <cs50.h>
 
+string prompt_name(void);
+int prompt_age(void);
+int prompt_phone_number(void);
+
 int main(void)
 {
-    n = prompt_name()
-    a = prompt_age()
-    p =prompt_phone_number()
-    print_details(n, a, p)
+    string n = prompt_name();
+    int a = prompt_age();
+    int p =prompt_phone_number();
+    print_details(n, a, p);
 }
 
+string prompt_name(void)
+{
+    string name = get_string("What's your name? ");
+    return name;
+}
+
+int prompt_age(void)
+{
+    int age = get_int("How old are you? ");
+    return age;
+}
+
+int prompt_phone_number(void)
+{
+    int phone_number = get_int("What's your phone number? ");
+    return phone_number;
+}
+
+void print_details(char n, int a, int p)
+{
+    printf("Hello, %s.", n);
+    printf("You are %i.", a);
+    printf("Your phone number is %i.", p);
+}
