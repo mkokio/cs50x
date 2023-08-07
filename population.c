4d2575cea88e5451fd7f2f69c979ca5b779b03aa
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-07 04:20:33 +0000

    Mon, Aug 7, 2023, 1:20 PM +09:00

--- population.c
+++ population.c
@@ -7,14 +7,26 @@
 
 int main(void)
 {
-    float c = get_float("How many llamas do you have? "); //current llamas
-    float n = get_float("How many llamas do you need? "); //needed llamas
+    //current llamas
+    int c = get_int("How many llamas do you have? ");
+
+    //needed llamas
+    int n;
+    do
+    {
+        get_int("How many llamas do you need? ");
+    }
+    while (n < c);
+
+    //do the math
     int y;
     for (y = 0; c < n; y++)
     {
-        printf("Year %i: %f llamas.\n", y, c);
+        printf("Year %i: %i llamas.\n", y, c);
         c = c + (c/3) - (c/4);
     }
+
+    //print result
     printf("After this many years: %i\n", y);
-    printf("You now this may llamas: %f\n", c);
+    printf("You now this may llamas: %i\n", c);
 }
