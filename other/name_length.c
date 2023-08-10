8fedf4e36182e961a34289ad9b2ec8b3daca35ae
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-10 10:42:43 +0000

    Thu, Aug 10, 2023, 7:42 PM +09:00

--- other/name_length.c
+++ other/name_length.c
@@ -5,7 +5,7 @@ int main(void)
 {
     string name = get_string("What's your first name? ");
     int n = 0;
-    printf("These are the binary codes to each letter of your name.\n");
+    printf("These are the binary codes to each letter of your name:\n");
     while (name[n] != '\0')
     {
         printf("%i ", name[n]);
