d0ae50ee007bfcdb4a0dabf1de0ab12a8391b8d7
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-10 09:50:38 +0000

    Thu, Aug 10, 2023, 6:50 PM +09:00

--- other/average.c
+++ other/average.c
@@ -1,15 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
-const int N = 3; //the length of your array; how many numbers do you want the average of?
+const int N = 3; // the length of your array; how many numbers do you want the average of?
 
 float get_average(int length, int array[]);
 
-int main (void)
+int main(void)
 {
     int numbers[N];
     printf("Let's get the average of %i numbers.\n", N);
-    for(int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
         numbers[i] = get_int("Write a number: ");
     }
