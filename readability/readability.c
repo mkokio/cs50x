c66d6a61e68781c60d2deeed2268671921178dbc
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-15 07:57:48 +0000

    Tue, Aug 15, 2023, 4:57 PM +09:00

--- readability/readability.c
+++ readability/readability.c
@@ -83,7 +83,6 @@ void reading_level(int c, int w, int s)
     double L = c / w * 100;
     double S = s / w * 100;
     int index = (int)round((0.0588 * L) - (0.296 * S) - 15.8);
-    printf("Index is: %i\n", index);
     if (index < 1)
     {
         printf("Before Grade 1.\n");
