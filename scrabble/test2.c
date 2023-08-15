5bafc4fa8ffdf5c100cb039a30d0d854d3d9fc95
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-15 01:59:52 +0000

    Tue, Aug 15, 2023, 10:59 AM +09:00

--- scrabble/test2.c
+++ scrabble/test2.c
@@ -5,13 +5,13 @@
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-char ALPHABET[] = {a, b, c, d, e, f, g, h, i, j, k, l ,m ,n, o, p, q, r, s, t, u, v, w, x, y, z};
+char ALPHABET[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
 int compute_score(string word);
 
 int main(void)
 {
-    string word = "cow"
-    printf(string[0])
+    string word = "cow";
+    printf(word[0]);
 }
 
