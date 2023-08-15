f141e0ccb8432fcf4c98c3f180aaf4c4a2619f94
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-15 01:25:06 +0000

    Tue, Aug 15, 2023, 10:25 AM +09:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -19,6 +19,18 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins.\n");
+    }
+    else if (score2 > score1)
+    {
+        printf("Player 2 wins.\n");
+    }
+    else
+    {
+        printf("It is a tie.\n")
+    }
 }
 
 int compute_score(string word)
