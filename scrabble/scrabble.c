673c8b628adec093ea1fed3c7fd53e74cdc507b5
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-15 02:57:19 +0000

    Tue, Aug 15, 2023, 11:57 AM +09:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -47,6 +47,7 @@ int compute_score(string word) //word is 'ab' for example
         if (word[n] >= 65 && word[n] <= 90)
         {
             word[n] += (32 - 97);
+            printf("%i letter is uppercase, position is %i\n", n, );
         }
         //use lowercase minus 97 to match real alphabet position
         else if (word[n] >= 97 && word[n] <= 122)
