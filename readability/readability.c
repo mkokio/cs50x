26d1e5a3d40654493953abc0d684cfa1dd05b305
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-15 07:05:59 +0000

    Tue, Aug 15, 2023, 4:05 PM +09:00

--- readability/readability.c
+++ readability/readability.c
@@ -82,7 +82,11 @@ int count_sentences(string text)
     return sent_count;
 }
 
-void reading_level(int c, int w, int s);
+void reading_level(int c, int w, int s)
 {
-
+    int L = 
 }
+
+//index = 0.0588 * L - 0.296 * S - 15.8
+//where L is the average number of letters per 100 words in the text,
+//and S is the average number of sentences per 100 words in the text.
