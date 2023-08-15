9494ea3c8f60574f640cc06826cca974b720ea96
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-15 01:35:22 +0000

    Tue, Aug 15, 2023, 10:35 AM +09:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -5,6 +5,7 @@
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+char ALPHABET[] = {a, b, c, d, e, f, g, h, i, j, k, l ,m ,n, o, p, q, r, s, t, u, v, w, x, y, z};
 
 int compute_score(string word);
 
@@ -39,7 +40,13 @@ int compute_score(string word)
     int score;
 
     word[0] //first letter of our word
-    
+    int n = 0;
+
+    hile (word[0] != ALPHABET[n])
+    {
+        n++;
+    }
+
 
     return score
 }
