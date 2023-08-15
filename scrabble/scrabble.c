9598623253aaf8a52efdd226e45aee942a6818a9
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-15 01:47:19 +0000

    Tue, Aug 15, 2023, 10:47 AM +09:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -42,7 +42,7 @@ int compute_score(string word)
     word[0] //first letter of our word
 
     //convert fully to lowercase
-    if (int word[0] < 91)
+    if (word[0] < 91)
     {
         word[0] += 32;
     }
