79aca5aa5bc0d149e5fc90caca88d7fb71cb7002
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-15 05:57:00 +0000

    Tue, Aug 15, 2023, 2:57 PM +09:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+// index = 0.0588 * L - 0.296 * S - 15.8
+// L is average number of letters per 100 words
+// S is average number of sentences per 100 words in the text
+
+int main(void)
 {
 
 }
