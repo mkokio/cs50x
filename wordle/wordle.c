15a8eb4c7c6d54a00a600b0b98c40661186efe22
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-16 01:22:51 +0000

    Wed, Aug 16, 2023, 10:22 AM +09:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -154,9 +154,9 @@ int check_word(string guess, int wordsize, int status[], string choice)
     for (int x = 0; guess[x] != '\0'; x++) // iterate over each letter of the guess DONE!
     {
         printf("x is %i, guess[x] is %c. ", x, guess[x]);
-        for (int y = 0; choice[y] != '\0'; y++)
+        for (int y = 0; choice[y] != '\0'; y++) // iterate over each letter of the choice
         {
-            if (guess[x] == choice[y])
+            if (guess[x] == choice[y]) // compare the current guess letter to the current choice letter
             {
                 score += 2; //EXACT
                 printf("Score is now %i.", score);
