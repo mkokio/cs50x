185148ebe9761a5a42dd8b1d0c6af532beb1f256
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-16 00:14:13 +0000

    Wed, Aug 16, 2023, 9:14 AM +09:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -47,6 +47,10 @@ int main(int argc, string argv[])
         printf("Error: wordsize must be either 5, 6, 7, or 8\n");
         return 1;
     }
+    else
+    {
+        wordsize = k;
+    }
 
 
     // open correct file, each file has exactly LISTSIZE words
