f46107f9409baf3de9ebfef27fd0dd92f3705fe6
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-16 01:11:09 +0000

    Wed, Aug 16, 2023, 10:11 AM +09:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -74,7 +74,7 @@ int main(int argc, string argv[])
     // pseudorandomly select a word for this game
     srand(time(NULL));
     string choice = options[rand() % LISTSIZE];
-
+    printf("%s", choice); //DELETE BEFORE SUBMIT
     // allow one more guess than the length of the word
     int guesses = wordsize + 1;
     bool won = false;
