e39fad73d1bab3d20fa3a931b914efe230b4d5a5
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-16 01:46:25 +0000

    Wed, Aug 16, 2023, 10:46 AM +09:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -163,7 +163,13 @@ int check_word(string guess, int wordsize, int status[], string choice)
                 printf("Score is now %i. ", score);
                 break;
             }
-            else if (guess[x])
+            else if (guess[x] == choice[y])
+            {
+                score += 1; //CLOSE
+                status[x] = 1;
+                printf("Score is now %i. ", score);
+                break;
+            }
         }
         //current status
         for (int g = 0; g < wordsize; g++)
