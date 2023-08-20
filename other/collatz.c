1edcbdfdb1c62fe65c0aaf59e85ae8a22c02bb5d
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-20 13:24:46 +0000

    Sun, Aug 20, 2023, 10:24 PM +09:00

--- other/collatz.c
+++ other/collatz.c
@@ -1,21 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void collatz(int n);
+int collatz(int n);
 
 int main(void)
 {
     int input = get_int("Type a whole number: ");
-    collatz(input);
-    //printf("Steps to get to 1: %i/n", total_steps);
+    int steps = collatz(input);
+    printf("Steps to get to 1: %i\n", steps);
 }
 
-void collatz(int n)
+int collatz(int n)
 {
     if (n == 1)
         return 0;
     else if ((n%2) == 0)
-        collatz(n/2);
+        return 1 + collatz(n/2);
     else
-        collatz(3*n + 1);
+        return 1 + collatz(3*n + 1);
 }
