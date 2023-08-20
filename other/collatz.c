1ccb5acc277e76195fc5d284182156ea99997793
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-20 13:00:14 +0000

    Sun, Aug 20, 2023, 10:00 PM +09:00

---
+++ other/collatz.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int collatz(int n);
+
+int main(void)
+{
+    int = get_int("Type a whole number: ")
+}
+
+int collatz(int n)
+{
+    if (n == 1)
+    {
+        //stop counting
+    }
+    
+}
