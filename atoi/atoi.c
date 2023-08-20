399401e33f940aef72ad3553c537c8da573bb68a
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-20 12:31:07 +0000

    Sun, Aug 20, 2023, 9:31 PM +09:00

--- atoi/atoi.c
+++ atoi/atoi.c
@@ -26,8 +26,13 @@ int main(void)
 int convert(string input)
 {
     // TODO
-    if (n <= 0)
-    {
-        return;
-    }
+    //Start by getting the index of the last char in the string (the char before the \0).
+    for (int i = 0; )
+    //Convert this char into its numeric value. Can you subtract some char to do this?
+
+    //Remove the last char from the string by moving the null terminator one position to the left.
+
+    //Return this value plus 10 times the integer value of the new shortened string.
+
+    //Remember you need a base case when creating a recursive function.
 }
