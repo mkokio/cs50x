ad17620885f584bef618bb31145f4945aca5e67b
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-20 13:36:25 +0000

    Sun, Aug 20, 2023, 10:36 PM +09:00

---
+++ other/test.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int convert(string input);
+
+int main(void)
+{
+    string input = get_string("Enter a positive integer: ");
+
+    int n = 0;
+    while (input[n] != '\0')
+    {
+        n++;
+    }
+    printf("Length of string: %i\n", n);
+    //n is now the length of the string
+
+    //Convert this char into its numeric value. Can you subtract some char to do this?
+    int input[n]
+
+    //Remove the last char from the string by moving the null terminator one position to the left.
+
+    //Return this value plus 10 times the integer value of the new shortened string.
+
+    //Remember you need a base case when creating a recursive function.
+}
