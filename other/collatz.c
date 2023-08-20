709eda2c6e001dd3c806ec41f2eb1f61902905e3
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-20 13:27:34 +0000

    Sun, Aug 20, 2023, 10:27 PM +09:00

--- other/collatz.c
+++ other/collatz.c
@@ -13,9 +13,18 @@ int main(void)
 int collatz(int n)
 {
     if (n == 1)
+    {
+        printf("n is %i\n", n);
         return 0;
+    }
     else if ((n%2) == 0)
+    {
+        printf("n is %i\n", n);
         return 1 + collatz(n/2);
+    }
     else
+    {
+        printf("n is %i\n", n);
         return 1 + collatz(3*n + 1);
+    }
 }
