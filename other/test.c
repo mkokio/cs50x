7d1243f867e8df809dc23804ead3475da37f57de
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-20 13:45:52 +0000

    Sun, Aug 20, 2023, 10:45 PM +09:00

--- other/test.c
+++ other/test.c
@@ -15,14 +15,14 @@ int main(void)
     {
         n++;
         int x = input[n];
-        printf("DEC of the last digit is %i\n", x)
+        printf("DEC of the last digit is %i\n", x);
     }
     printf("Length of string: %i\n", n);
     //n is now the length of the string
 
     //Convert this char into its numeric value. Can you subtract some char to do this?
-    int x = input[n];
-    printf("DEC of the last digit is %i\n", x);
+    //int x = input[n];
+    //printf("DEC of the last digit is %i\n", x);
 
     //Remove the last char from the string by moving the null terminator one position to the left.
 
