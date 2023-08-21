ab505cae119d7fd700de516d7810698d41052563
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-21 01:12:48 +0000

    Mon, Aug 21, 2023, 10:12 AM +09:00

--- sort/sorttest.c
+++ sort/sorttest.c
@@ -4,12 +4,13 @@
 
 int main() {
     printf("Running the compiled binary...\n");
+    string time = "time";
     string binary = "./sort1";
     string filename = "random5000.txt";
     char command[50]; // Make sure the buffer is large enough
 
     // Create the command string
-    snprintf(command, sizeof(command), "%s %s", binary, filename);
+    snprintf(command, sizeof(command), "%s %s %s", time, binary, filename);
 
     system(command);
     printf("Finished running the compiled binary.\n");
