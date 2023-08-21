61e41f19adaf8e561e38a2c61774e52a60d0a17c
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-21 00:51:18 +0000

    Mon, Aug 21, 2023, 9:51 AM +09:00

---
+++ sort/sorttest.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Running the compiled binary...\n");
+    system("./sort1"); // Replace "compiled_binary" with the actual binary name
+    printf("Finished running the compiled binary.\n");
+
+    return 0;
+}
