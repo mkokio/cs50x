2346bc6abbacc275a73da902bb4173bf32afd759
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-21 01:19:05 +0000

    Mon, Aug 21, 2023, 10:19 AM +09:00

--- sort/sorttest.c
+++ sort/sorttest.c
@@ -3,7 +3,6 @@
 #include <cs50.h>
 
 int main() {
-    printf("Running the compiled binary...\n");
     string binary = "./sort1";
     string filename = "random5000.txt";
     char command[100]; // Make sure the buffer is large enough
@@ -12,7 +11,5 @@ int main() {
     snprintf(command, sizeof(command), "time %s %s", binary, filename);
 
     system(command);
-    printf("Finished running the compiled binary.\n");
-
     return 0;
 }
