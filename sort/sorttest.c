808be9471783cb6e3ddda0a33e10168cd190f407
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-21 00:54:42 +0000

    Mon, Aug 21, 2023, 9:54 AM +09:00

--- sort/sorttest.c
+++ sort/sorttest.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <cs50.h>
 
 int main() {
     printf("Running the compiled binary...\n");
-    string binary = "./sort1";
-    system("%s", binary); // Replace "compiled_binary" with the actual binary name
+    system("./sort1 sorted5000.txt");
     printf("Finished running the compiled binary.\n");
 
     return 0;
