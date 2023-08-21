6351fc8360923fd3742d44fd5429d5fe25e10e85
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-21 01:25:03 +0000

    Mon, Aug 21, 2023, 10:25 AM +09:00

--- sort/sorttest.c
+++ sort/sorttest.c
@@ -3,14 +3,15 @@
 #include <cs50.h>
 
 int main() {
-    string binary = "./sort1";
-    string filename = "random5000.txt";
-    char command[100]; // Make sure the buffer is large enough
+    //string binary = "./sort1";
+    //string filename = "random5000.txt";
+    //char command[100]; // Make sure the buffer is large enough
 
     // Create the command string
-    snprintf(command, sizeof(command), "time %s %s", binary, filename);
-    printf("%s",command);
-    string full_comand = (string) command;
-    system(command);
+    //snprintf(command, sizeof(command), "time %s %s", binary, filename);
+    //printf("%s",command);
+    //string full_comand = (string) command;
+    //system(command);
+    sytem(time)
     return 0;
 }
