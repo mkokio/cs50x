736217b11d1c17e42e2a72549e4b05e4a47ec90b
Marc <108322460+mkokio@users.noreply.github.com>
2023-08-27 11:41:44 +0000

    Sun, Aug 27, 2023, 8:41 PM +09:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -10,8 +10,7 @@ typedef struct
 {
     string name;
     int votes;
-}
-candidate;
+} candidate;
 
 // Array of candidates
 candidate candidates[MAX];
@@ -39,16 +38,16 @@ int main(int argc, string argv[])
         printf("Maximum number of candidates is %i\n", MAX);
         return 2;
     }
-    for (int i = 0; i < candidate_count; i++)
+    for (int i = 0; i < candidate_count; i++) // copies command-line arguments into the array candidates... available globally?
     {
-        candidates[i].name = argv[i + 1];
+        candidates[i].name = argv[i + 1]; // because argv[i] is ./plurality
         candidates[i].votes = 0;
     }
 
     int voter_count = get_int("Number of voters: ");
 
     // Loop over all voters
-    for (int i = 0; i < voter_count; i++)
+    for (int i = 0; i < voter_count; i++) // let every voter type in a vote
     {
         string name = get_string("Vote: ");
 
@@ -67,6 +66,18 @@ int main(int argc, string argv[])
 bool vote(string name)
 {
     // TODO
+    // search if 'name' is in candidates.name[0 to argc-1]
+    for (int i = 0; i < candidate_count; i++)
+    {
+        // repeat until end, starting at first element THIS IS LINEAR SEARCH
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            // if first element is 'name', stop and add vote
+            candidates[i].votes++;
+            return true;
+        }
+    }
+    // if 'name' is not in argv[], return false (an invalid vote)
     return false;
 }
 
@@ -74,5 +85,30 @@ bool vote(string name)
 void print_winner(void)
 {
     // TODO
+    // The function should print out the name of the candidate who received
+    // the most votes in the election, and then print a newline.
+    // It is possible that the election could end in a tie if multiple
+    // candidates each have the maximum number of votes. In that case,
+    // you should output the names of each of the winning candidates, each on a separate line.
+    // (doing practice MAX before this part)
+    // set current max
+    int max = 0;
+    // iterate through each candidate
+    for (int i = 0; i < candidate_count; i++)
+    {
+        // check if element is bigger than current max, if so, make current max
+        if (candidates[i].votes > max)
+        {
+            max = candidates[i].votes;
+        }
+    }
+    // iterate through each candidate
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes == max)
+        {
+            printf("%s\n", candidates[i].name);
+        }
+    }
     return;
 }
