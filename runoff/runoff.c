f20ae71b5493fd0a5b385f2552191967ba33da2f
Marc <108322460+mkokio@users.noreply.github.com>
2023-09-02 02:36:57 +0000

    Sat, Sep 2, 2023, 11:36 AM +09:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -132,11 +132,11 @@ bool vote(int voter, int rank, string name)
     // look for a candidate called 'name'
     for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(*candidate[i].name, name) == 0)
+        if (strcmp(candidates[i].name, name) == 0)
         {
             printf("name found\n");
             // update preferences so they are the voter's rank prefence
-            
+
             // and return true
             return true;
         }
