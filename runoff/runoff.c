0d6d5e392c8b7d7df54055fc9ba40ab2123a6890
Marc <108322460+mkokio@users.noreply.github.com>
2023-09-02 01:02:57 +0000

    Sat, Sep 2, 2023, 10:02 AM +09:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -128,6 +128,12 @@ int main(int argc, string argv[])
 bool vote(int voter, int rank, string name)
 {
     // TODO
+    // look for a condidate called 'name'
+    
+
+    // update preferences so they are the voter's rank prefence
+
+    // and return true
     return false;
 }
 
