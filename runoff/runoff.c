f869cb7b6743a70e451e222d62e97a14d64f2f36
Marc <108322460+mkokio@users.noreply.github.com>
2023-09-03 23:39:18 +0000

    Mon, Sep 4, 2023, 8:39 AM +09:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -135,7 +135,7 @@ bool vote(int voter, int rank, string name)
         if (strcmp(candidates[i].name, name) == 0)
         {
             // update preferences so they are the voter's rank prefence
-            candidates[preferences]
+            candidates[preferences[i][0]].votes++;
             printf("Voter '%i' preference has been set to %s./n", voter, prefences[voter][rank]);
             // and return true
             return true;
