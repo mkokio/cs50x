aa998fb7b03b735682fce833bb04ca91e8ab41a7
Marc <108322460+mkokio@users.noreply.github.com>
2023-09-04 07:16:29 +0000

    Mon, Sep 4, 2023, 4:16 PM +09:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -232,7 +232,7 @@ bool is_tie(int min)
     {
         if(!candidates[i].eliminated && candidates[i].votes == min)
         {
-            return true;
+            
         }
     }
     return false;
