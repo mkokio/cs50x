c499b90a42ec11166d8ec2c6a5b0326a05ad824e
Marc <108322460+mkokio@users.noreply.github.com>
2023-09-21 00:06:26 +0000

    Thu, Sep 21, 2023, 9:06 AM +09:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -78,11 +78,23 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         {
             for (int j = 0; j < width; j++)
             {
-                if (i = 0)
+                if (i == 0) // Top Row
                     {
-                        if (j = 0)
+                        if (j == 0) // First Pixel
                         {
-                            image[i][j] = round((tmp[i][j]+tmp[i+1][j]+tmp[i][j+1]+tmp[i+1][j+1])/4)
+                            image[i][j].rgbtBlue = round((tmp[i][j].rgbtBlue+tmp[i][j+1].rgbtBlue+tmp[i+1][j].rgbtBlue+tmp[i+1][j+1].rgbtBlue)/4);
+                            image[i][j].rgbtGreen = round((tmp[i][j].rgbtGreen+tmp[i][j+1].rgbtGreen+tmp[i+1][j].rgbtGreen+tmp[i+1][j+1].rgbtGreen)/4);
+                            image[i][j].rgbtRed = round((tmp[i][j].rgbtRed+tmp[i][j+1].rgbtRed+tmp[i+1][j].rgbtRed+tmp[i+1][j+1].rgbtRed)/4);
+                        }
+                        else if (j < width) // Middle but not last pixel
+                        {
+                            image[i][j].rgbtBlue = round((tmp[i][j].rgbtBlue+tmp[i][j+1].rgbtBlue+tmp[i+1][j].rgbtBlue+tmp[i+1][j+1].rgbtBlue)+tmp[i][j-1].rgbtBlue+tmp[i+1][j-1].rgbtBlue)/6);
+                            image[i][j].rgbtGreen = round((tmp[i][j].rgbtGreen+tmp[i][j+1].rgbtGreen+tmp[i+1][j].rgbtGreen+tmp[i+1][j+1].rgbtGreen)+tmp[i][j-1].rgbtGreen+tmp[i+1][j-1].rgbtGreen)/6);
+                            image[i][j].rgbtRed = round((tmp[i][j].rgbtRed+tmp[i][j+1].rgbtRed+tmp[i+1][j].rgbtRed+tmp[i+1][j+1].rgbtRed)+tmp[i][j-1].rgbtBlue+tmp[i+1][j-1].rgbtBlue)/6);
+                        }
+                        else // Last Pixel on top row
+                        {
+
                         }
                     }
             }
