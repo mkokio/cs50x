ff6f39436e5a24d76062ad109a7d9a4083ae41d8
Marc <108322460+mkokio@users.noreply.github.com>
2023-09-21 01:20:06 +0000

    Thu, Sep 21, 2023, 10:20 AM +09:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -113,6 +113,12 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                         image[i][j].rgbtGreen = round((tmp[i][j].rgbtGreen+tmp[i][j+1].rgbtGreen+tmp[i+1][j].rgbtGreen+tmp[i+1][j+1].rgbtGreen+tmp[i-1][j].rgbtGreen+tmp[i-1][j+1].rgbtGreen+tmp[i+1][j-1].rgbtGreen+tmp[i][j-1].rgbtGreen+tmp[i-1][j+1].rgbtGreen))/9);
                         image[i][j].rgbtRed = round((tmp[i][j].rgbtRed+tmp[i][j+1].rgbtRed+tmp[i+1][j].rgbtRed+tmp[i+1][j+1].rgbtRed+tmp[i-1][j].rgbtRed+tmp[i-1][j+1].rgbtRed+tmp[i+1][j-1].rgbtRed+tmp[i][j-1].rgbtRed+tmp[i-1][j+1].rgbtRed))/9);
                     }
+                    else //last pixel
+                    {
+                        image[i][j].rgbtBlue = round((tmp[i][j].rgbtBlue+tmp[i][j+1].rgbtBlue+tmp[i+1][j].rgbtBlue+tmp[i+1][j+1].rgbtBlue+tmp[i-1][j].rgbtBlue+tmp[i-1][j+1].rgbtBlue+tmp[i+1][j-1].rgbtBlue+tmp[i][j-1].rgbtBlue+tmp[i-1][j+1].rgbtBlue))/9);
+                        image[i][j].rgbtGreen = round((tmp[i][j].rgbtGreen+tmp[i][j+1].rgbtGreen+tmp[i+1][j].rgbtGreen+tmp[i+1][j+1].rgbtGreen+tmp[i-1][j].rgbtGreen+tmp[i-1][j+1].rgbtGreen+tmp[i+1][j-1].rgbtGreen+tmp[i][j-1].rgbtGreen+tmp[i-1][j+1].rgbtGreen))/9);
+                        image[i][j].rgbtRed = round((tmp[i][j].rgbtRed+tmp[i][j+1].rgbtRed+tmp[i+1][j].rgbtRed+tmp[i+1][j+1].rgbtRed+tmp[i-1][j].rgbtRed+tmp[i-1][j+1].rgbtRed+tmp[i+1][j-1].rgbtRed+tmp[i][j-1].rgbtRed+tmp[i-1][j+1].rgbtRed))/9);
+                    }
                 }
             }
         }
