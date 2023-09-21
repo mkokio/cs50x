30dcf964ab7e64656031bbb455debad5ae3704e7
Marc <108322460+mkokio@users.noreply.github.com>
2023-09-21 02:53:26 +0000

    Thu, Sep 21, 2023, 11:53 AM +09:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -81,7 +81,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             {
                 if (j == 0) // First Pixel
                 {
-                    image[i][j].rgbtBlue = round((tmp[i][j].rgbtBlue + tmp[i][j + 1].rgbtBlue + tmp[i + 1][j].rgbtBlue + tmp[i +1][j + 1].rgbtBlue)/4);
+                    image[i][j].rgbtBlue = round((tmp[i][j].rgbtBlue + tmp[i][j + 1].rgbtBlue + tmp[i + 1][j].rgbtBlue + tmp[i + 1][j + 1].rgbtBlue) / 4);
                     image[i][j].rgbtGreen = round((tmp[i][j].rgbtGreen+tmp[i][j+1].rgbtGreen+tmp[i+1][j].rgbtGreen+tmp[i+1][j+1].rgbtGreen)/4);
                             image[i][j].rgbtRed = round((tmp[i][j].rgbtRed+tmp[i][j+1].rgbtRed+tmp[i+1][j].rgbtRed+tmp[i+1][j+1].rgbtRed)/4);
                 }
