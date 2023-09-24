c621ef6f63d62efdf2fd0b09b9c3db00bae51c4b
Marc <108322460+mkokio@users.noreply.github.com>
2023-09-24 07:28:33 +0000

    Sun, Sep 24, 2023, 4:28 PM +09:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -63,8 +63,30 @@ int check_format(WAVHEADER header)
 }
 
 
-int get_block_size(WAVHEADER header)
+int get_block_size(WAVHEADER header) //bytes per sample X number of channels
 {
     // TODO #7
+    int bloc_size = header.
     return 0;
 }
+// return an integer representing the block size of the given WAV file,
+// in bytes. We can think of a block as a unit of auditory data. For audio,
+// we calculate the size of each block with the following calculation:
+// number of channels multiplied by bytes per sample. Luckily, the header
+// contains all the information we need to compute these values.
+// Be sure to reference the Background section for a more in-depth explanation
+// as to what these values mean and how they are stored. See too wav.h, to determine which members of WAVHEADER might be useful.
+// Notice that one of the members of WAVHEADER is bitsPerSample. But to calculate block size, you’ll need bytes per sample!
+BYTE   chunkID[4];
+DWORD  chunkSize;
+BYTE   format[4];
+BYTE   subchunk1ID[4];
+DWORD  subchunk1Size;
+WORD   audioFormat;
+WORD   numChannels;
+DWORD  sampleRate;
+DWORD  byteRate;
+WORD   blockAlign;
+WORD   bitsPerSample;
+BYTE   subchunk2ID[4];
+DWORD  subchunk2Size;
