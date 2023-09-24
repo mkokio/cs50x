95af4afb52194a809d0fa19b3f912fabbb11c2e7
Marc <108322460+mkokio@users.noreply.github.com>
2023-09-24 12:58:38 +0000

    Sun, Sep 24, 2023, 9:58 PM +09:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -64,10 +64,10 @@ int main(int argc, char *argv[])
 
     fseek(ptr1, 0, SEEK_END); //offest is zero from the end of the file
     int endoffileposition = ftell(ptr1);
-    for (int i = endoffileposition - 1; i >= endofheaderposition; i++)
+    for (int i = endoffileposition - 1; i >= endofheaderposition; i--)
     {
         //copy (write) the last character of ptr1 into the first position of ptr2
-        fseek(ptr1, i, SEEK_SET); // move to the ith position (one less each loop)
+        fseek(ptr1, i * block_size, SEEK_SET); // move to the ith position (one less each loop)
         char ch = fgetc(ptr1); // read and store that character as ch
         fputc(ch, ptr2); // put that character in place
     }
