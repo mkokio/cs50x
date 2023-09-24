c683b9d2d06cb8a02d04a07a33d8ab755d938a7b
Marc <108322460+mkokio@users.noreply.github.com>
2023-09-24 11:53:36 +0000

    Sun, Sep 24, 2023, 8:53 PM +09:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -56,10 +56,10 @@ int main(int argc, char *argv[])
 // be pointing at the location where the read concluded. In other words, the input pointer
 // may need to be moved back two block sizes after each fread, one to move back to where
 // the fread began, and the second to move to the previous, unread block.
-   for (int i = 0; i > -)
-   fseek(ptr1, i, SEEK_END); //offest is zero from the end of the file
+
+   fseek(ptr1, 0, SEEK_END); //offest is zero from the end of the file
    int endoffileposition = ftell(ptr1);
-   printf("%i\n", endoffileposition);
+   for (int i = 0; i > - endoffileposition + endofheaderposition; i--)
 
 
 fclose(ptr1);
