58338d676879527e2ed109da821708793505b13e
Marc <108322460+mkokio@users.noreply.github.com>
2024-02-13 07:16:23 +0000

    Tue, Feb 13, 2024, 4:16 PM +09:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -8,13 +8,12 @@ while True:
 coins = 0
 
 types = [.25, .10, .05, .01]
-total_coins = 0
 
 while True:
     if change < 0:
         break
     for type in types:
-        coins += int(change/type)
+        coins == int(change/type)
         print(f"{type} coins: {coins}")
         change = round(change - (coins*type),2)
         print(f"change: {change}")
