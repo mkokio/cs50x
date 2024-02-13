eb796142cfbe0c1b6002f47d37f929087ded5181
Marc <108322460+mkokio@users.noreply.github.com>
2024-02-13 23:46:20 +0000

    Wed, Feb 14, 2024, 8:46 AM +09:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,3 +1,8 @@
 from cs50 import get_str
 
 text = get_str("Text: ")
+
+# 0.0588 * L - 0.296 * S - 15.8
+
+#L is the average number of letters per 100 words in the text
+#S is the average number of sentences per 100 words in the text.
