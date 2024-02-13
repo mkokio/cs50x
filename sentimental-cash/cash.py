44ef2e97315ecfc17f9cf5427227949052d6f700
Marc <108322460+mkokio@users.noreply.github.com>
2024-02-13 06:29:30 +0000

    Tue, Feb 13, 2024, 3:29 PM +09:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -8,19 +8,13 @@ while True:
 coins = 0
 
 coins += int(change/.25)
+print(f"quarters: {coins}")
 change = change - (int(change/.25)*.25)
-print(coins)
-print(change)
 coins += int(change/.10)
-change = clearchange - (int(change/.10)*.10)
-print(coins)
-print(change)
+change = change - (int(change/.10)*.10)
 coins += int(change/.05)
 change = change - (int(change/.05)*.05)
-print(coins)
-print(change)
 coins += int(change/.01)
 change += change - (int(change/.01)*.01)
-
 
 print(coins)
