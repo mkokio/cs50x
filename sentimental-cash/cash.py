7a9ab797681e8c25052f690b5df161851a543e8d
Marc <108322460+mkokio@users.noreply.github.com>
2024-02-13 06:26:45 +0000

    Tue, Feb 13, 2024, 3:26 PM +09:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -9,11 +9,18 @@ coins = 0
 
 coins += int(change/.25)
 change = change - (int(change/.25)*.25)
+print(coins)
+print(change)
 coins += int(change/.10)
 change = change - (int(change/.10)*.10)
+print(coins)
+print(change)
 coins += int(change/.05)
 change = change - (int(change/.05)*.05)
+print(coins)
+print(change)
 coins += int(change/.01)
 change += change - (int(change/.01)*.01)
+
 
 print(coins)
