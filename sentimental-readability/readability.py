0f4cbe50fcc59136fece2874a7fec3aec8d9d7b6
Marc <108322460+mkokio@users.noreply.github.com>
2024-02-13 23:50:06 +0000

    Wed, Feb 14, 2024, 8:50 AM +09:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -5,8 +5,12 @@ x = 3
 text = get_string("Text: ")
 
 # 0.0588 * L - 0.296 * S - 15.8
-
 #L is the average number of letters per 100 words in the text
 #S is the average number of sentences per 100 words in the text.
+
+total_words =
+total_sentences =
+
+L = 
 
 print(f"Grade {x}")
