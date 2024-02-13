112f670f3430b6fe16d6523e40f542cffffe1aa5
Marc <108322460+mkokio@users.noreply.github.com>
2024-02-13 07:04:14 +0000

    Tue, Feb 13, 2024, 4:04 PM +09:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -6,9 +6,15 @@ while True:
         break
 
 quarters = 0
+dimes = 0
 quarters += int(change/.25)
 print(f"quarters: {quarters}")
 print(f"change: {change}")
 print(quarters*.25)
 print(f"Remaining money after quarters: {round(change - (quarters*.25),2)}")
 
+quarters += int(change/.25)
+print(f"dimes: {dimes}")
+print(f"change: {change}")
+print(dimes*.10)
+print(f"Remaining money after quarters: {round(change - (dimes*.10),2)}")
