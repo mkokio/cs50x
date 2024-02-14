b1690f1547496c3f3b0ecc36799e27f7963ba1cc
Marc <108322460+mkokio@users.noreply.github.com>
2024-02-14 01:59:18 +0000

    Wed, Feb 14, 2024, 10:59 AM +09:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -5,7 +5,8 @@ text = get_string("Text: ")
 # 0.0588 * L - 0.296 * S - 15.8
 #L is the average number of letters per 100 words in the text
 #S is the average number of sentences per 100 words in the text.
-
+text_length = len(text)
+print(text_length)
 letters = 24
 words = 8
 sentences = 4
