f860df567ea2bbb7a340a17473147741503626eb
Marc <108322460+mkokio@users.noreply.github.com>
2024-02-14 02:48:13 +0000

    Wed, Feb 14, 2024, 11:48 AM +09:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -2,11 +2,12 @@ from cs50 import get_string
 
 text = get_string("Text: ")
 
+
 def count(text):
     punctuation = (" ", ",", ";", ":", ".", "!", "?")
     characters = 0
     words = 1
-    sentences =0
+    sentences = 0
     for c in text:
         if c not in punctuation:
             characters += 1
@@ -18,16 +19,16 @@ def count(text):
 
 
 def reading_level(characters, words, sentences):
-
-    L = characters / words * 100;
-    S = sentences / words * 100;
-    index = (0.0588 * L - 0.296 * S - 15.8);
+    L = characters / words * 100
+    S = sentences / words * 100
+    index = (0.0588 * L - 0.296 * S - 15.8)
 
     if index < 1:
         print("Before Grade 1")
     elif (1 <= index <= 16):
         print(f"Grade {round(index)}")
     else:
-        print("Grade 16+");
+        print("Grade 16+")
+
 
 reading_level(*count(text))
