99a1d8eb671b660271dd9b8f852b85661b3c328a
Marc <108322460+mkokio@users.noreply.github.com>
2024-02-14 07:21:19 +0000

    Wed, Feb 14, 2024, 4:21 PM +09:00

--- dna/dna.py
+++ dna/dna.py
@@ -34,7 +34,7 @@ def main():
     # TODO: Check database for matching profiles
     for entry in entries:
         for value in entry.values():
-            print(value)
+            if value
 
             #if #the dictionary's 1st, 2nd, 3rd value are the same as 'sequence' list, return the 0th value
 
