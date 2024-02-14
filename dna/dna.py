1de93fe5aabab8070668a58ac4aad2d1337b5c3a
Marc <108322460+mkokio@users.noreply.github.com>
2024-02-14 03:13:06 +0000

    Wed, Feb 14, 2024, 12:13 PM +09:00

--- dna/dna.py
+++ dna/dna.py
@@ -10,6 +10,8 @@ def main():
         return False
 
     # TODO: Read database file into a variable
+    with open(sys.argv[2]) as file:
+        
 
     # TODO: Read DNA sequence file into a variable
 
