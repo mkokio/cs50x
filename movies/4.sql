68a6b4054e4e16aa79ab48ccd5d7b403263a4bc1
Marc <108322460+mkokio@users.noreply.github.com>
2024-03-08 04:11:08 +0000

    Fri, Mar 8, 2024, 1:11 PM +09:00

--- movies/4.sql
+++ movies/4.sql
@@ -1,18 +1,1 @@
-
-write a SQL query to determine the number of movies with an IMDb rating of 10.0.
-
-Your query should output a table with a single column and a single row
-(not counting the header) containing the number of movies with a 10.0 rating.
-
-CREATE TABLE movies (
-    id INTEGER,
-    title TEXT NOT NULL,
-    year NUMERIC,
-    PRIMARY KEY(id)
-);
-
-CREATE TABLE ratings (
-    movie_id INTEGER NOT NULL,
-    rating REAL NOT NULL,
-    votes INTEGER NOT NULL,
-    FOREIGN KEY(movie_id) REFERENCES movies(id)
+SELECT COUNT(rating) FROM ratings WHERE rating = 10.0;
