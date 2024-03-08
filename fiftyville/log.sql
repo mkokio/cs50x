2b61f52271fff8cc16ede83795f359a5cb67121e
Marc <108322460+mkokio@users.noreply.github.com>
2024-03-08 12:27:23 +0000

    Fri, Mar 8, 2024, 9:27 PM +09:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -9,6 +9,7 @@ SELECT id, name, transcript FROM interviews WHERE day = 28 AND month = 7 AND tra
 -- | 162 | Eugene  | I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.                                                                                                 |
 -- | 163 | Raymond | As the thief was leaving the bakery, they called someone who talked to them for less than a minute. In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket. |
 SELECT id, hour, minute, activity, license_plate FROM bakery_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25;
+-- We know it's one of these license plates
 /*+-----+------+--------+----------+---------------+
 | 260 | 10   | 16     | exit     | 5P2BI95       |
 | 261 | 10   | 18     | exit     | 94KL13X       |
@@ -31,6 +32,7 @@ SELECT DISTINCT atm_location FROM atm_transactions;
 +----------------------+*/
 SELECT id, account_number, month, day, transaction_type, amount FROM atm_transactions WHERE atm_location = 'Leggett Street' AND month = 7 AND day = 2
 8;
+-- We know it's one of these account numbers
 /*+-----+----------------+-------+-----+------------------+--------+
 | id  | account_number | month | day | transaction_type | amount |
 +-----+----------------+-------+-----+------------------+--------+
@@ -45,6 +47,7 @@ SELECT id, account_number, month, day, transaction_type, amount FROM atm_transac
 | 336 | 26013199       | 7     | 28  | withdraw         | 35     |
 +-----+----------------+-------+-----+------------------+--------+*/
 SELECT id, caller, receiver, duration FROM phone_calls WHERE month = 7 AND day = 28 AND duration < 60;
+-- We know it's one of these callers, and the 
 /*+-----+----------------+----------------+----------+
 | id  |     caller     |    receiver    | duration |
 +-----+----------------+----------------+----------+
