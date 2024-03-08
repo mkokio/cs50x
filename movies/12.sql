ecb1eef31a47863dc7d8ab24735b156d7f103174
Marc <108322460+mkokio@users.noreply.github.com>
2024-03-08 06:29:03 +0000

    Fri, Mar 8, 2024, 3:29 PM +09:00

---
+++ movies/12.sql
@@ -0,0 +1,8 @@
+SELECT titles FROM movies, stars, people
+WHERE 
+
+list the titles of all movies in which both Bradley Cooper and Jennifer Lawrence starred.
+
+    Your query should output a table with a single column for the title of each movie.
+    You may assume that there is only one person in the database with the name Bradley Cooper.
+    You may assume that there is only one person in the database with the name Jennifer Lawrence.
