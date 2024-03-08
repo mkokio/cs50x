e1eea4b357c1c03d6ec7f6c56c99bd86f988d7c1
Marc <108322460+mkokio@users.noreply.github.com>
2024-03-08 07:05:45 +0000

    Fri, Mar 8, 2024, 4:05 PM +09:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,7 +1,7 @@
 SELECT title FROM movies, stars, people
 WHERE id IN
 (SELECT movie_id FROM stars WHERE person_id =
-  ()
+  (SELECT if FROM people WHERE name = 'Bradley Lawrence')
 WHERE movies.id = stars.movie_id
 AND people.id = stars.person_id
 AND name IN ('Bradley Cooper')
