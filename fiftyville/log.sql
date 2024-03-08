b94d6966d0e4bb02367aad513a53b3220af3824c
Marc <108322460+mkokio@users.noreply.github.com>
2024-03-08 11:59:01 +0000

    Fri, Mar 8, 2024, 8:59 PM +09:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -8,7 +8,7 @@ SELECT id, name, transcript FROM interviews WHERE day = 28 AND month = 7 AND tra
 -- | 161 | Ruth    | Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away. If you have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time frame.                                                          |
 -- | 162 | Eugene  | I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.                                                                                                 |
 -- | 163 | Raymond | As the thief was leaving the bakery, they called someone who talked to them for less than a minute. In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket. |
-SELECT id, activity, license_plate FROM bakery_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 20;
+SELECT id, hour, minute, activity, license_plate FROM bakery_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25;
 -- +-----+----------+---------------+
 -- | id  | activity | license_plate |
 -- +-----+----------+---------------+
