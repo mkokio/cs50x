7fb5526977cdf8eb94875006a350df1911015231
Marc <108322460+mkokio@users.noreply.github.com>
2024-03-08 11:50:32 +0000

    Fri, Mar 8, 2024, 8:50 PM +09:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -4,3 +4,7 @@ SELECT id, description FROM crime_scene_reports WHERE month = 7 AND day = 28;
 -- Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery.
 -- Interviews were conducted today with three witnesses who were present at the time
 --– each of their interview transcripts mentions the bakery.
+SELECT id, name, transcript FROM interviews WHERE day = 28 AND month = 7 AND transcript LIKE '%bakery%';
+-- | 161 | Ruth    | Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away. If you have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time frame.                                                          |
+-- | 162 | Eugene  | I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.                                                                                                 |
+-- | 163 | Raymond | As the thief was leaving the bakery, they called someone who talked to them for less than a minute. In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket. |
