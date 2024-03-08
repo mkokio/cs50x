7e5a85a62b7c389aeb6baa4c1a1c68f377863b79
Marc <108322460+mkokio@users.noreply.github.com>
2024-03-08 06:34:12 +0000

    Fri, Mar 8, 2024, 3:34 PM +09:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,7 +1,8 @@
 SELECT title FROM movies, stars, people
 WHERE movies.id = stars.movie_id
 AND people.id = stars.person_id
-AND name IN ('Bradley Cooper', 'Jennifer Lawrence');
+AND name IN ('Bradley Cooper')
+AND name IN ('Jennifer Lawrence');
 
 list the titles of all movies in which both Bradley Cooper and Jennifer Lawrence starred.
 
