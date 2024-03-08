422a0cffd9231fe13a45d1b186af953097cf28b3
Marc <108322460+mkokio@users.noreply.github.com>
2024-03-08 07:33:59 +0000

    Fri, Mar 8, 2024, 4:33 PM +09:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,5 +1,5 @@
 SELECT name FROM people WHERE people.id IN
-(SELECT stars.person_id FROM people, movies, stars
+(SELECT movies.id, people.name FROM people, movies, stars
 WHERE movies.id = stars.movie_id
 AND people.id = stars.person_id
 AND name ='Kevin Bacon'
