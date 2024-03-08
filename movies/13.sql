3f9317c033cf26e62944c3b6037028a29a14451a
Marc <108322460+mkokio@users.noreply.github.com>
2024-03-08 07:38:09 +0000

    Fri, Mar 8, 2024, 4:38 PM +09:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,6 +1,6 @@
 SELECT name FROM people, movies, stars
 WHERE movies.id = stars.movie_id
-AND people.id = stars.person_id AND movie.id IN
+AND people.id = stars.person_id AND movies.id IN
 (SELECT movies.id FROM people, movies, stars
 WHERE movies.id = stars.movie_id
 AND people.id = stars.person_id
