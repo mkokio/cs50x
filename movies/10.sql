47459655fe2d3bd5285a3c34bfb347c74e1bf964
Marc <108322460+mkokio@users.noreply.github.com>
2024-03-08 06:13:20 +0000

    Fri, Mar 8, 2024, 3:13 PM +09:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,6 +1,1 @@
-
-list the names of all people who have directed a movie that received a rating of at least 9.0.
-
-    Your query should output a table with a single column for the name of each person.
-    If a person directed more than one movie that received a rating of at least 9.0,
-    they should only appear in your results once.
+SELECT  DISTINCT name FROM movies, people, ratings, directors WHERE movies.id = ratings.movie_id AND rating >= 9.0 AND people.id = directors.person_id AND directors.movie_id = movies.id;
