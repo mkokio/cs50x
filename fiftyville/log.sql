e095b2ea75a17c8e2cc0af40c441e308cf4a5dcb
Marc <108322460+mkokio@users.noreply.github.com>
2024-03-08 12:00:08 +0000

    Fri, Mar 8, 2024, 9:00 PM +09:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -9,12 +9,13 @@ SELECT id, name, transcript FROM interviews WHERE day = 28 AND month = 7 AND tra
 -- | 162 | Eugene  | I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.                                                                                                 |
 -- | 163 | Raymond | As the thief was leaving the bakery, they called someone who talked to them for less than a minute. In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket. |
 SELECT id, hour, minute, activity, license_plate FROM bakery_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25;
--- +-----+----------+---------------+
--- | id  | activity | license_plate |
--- +-----+----------+---------------+
--- | 260 | exit     | 5P2BI95       |
--- | 261 | exit     | 94KL13X       |
--- | 262 | exit     | 6P58WS2       |
--- | 263 | exit     | 4328GD8       |
--- | 264 | exit     | G412CB7       |
--- +-----+----------+---------------+
+/*+-----+------+--------+----------+---------------+
+| 260 | 10   | 16     | exit     | 5P2BI95       |
+| 261 | 10   | 18     | exit     | 94KL13X       |
+| 262 | 10   | 18     | exit     | 6P58WS2       |
+| 263 | 10   | 19     | exit     | 4328GD8       |
+| 264 | 10   | 20     | exit     | G412CB7       |
+| 265 | 10   | 21     | exit     | L93JTIZ       |
+| 266 | 10   | 23     | exit     | 322W7JE       |
+| 267 | 10   | 23     | exit     | 0NTHK55       |
++-----+------+--------+----------+---------------+*/
