d33bc982dd630433234305ff891dc0df169a29cc
Marc <108322460+mkokio@users.noreply.github.com>
2024-03-08 11:40:54 +0000

    Fri, Mar 8, 2024, 8:40 PM +09:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,6 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
-
+SELECT id, description FROM crime_scene_reports WHERE month = 7 AND day = 28;
+-- Now I know the crime_scene_reports.id is 295
+-- Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery.
+-- Interviews were conducted today with three witnesses who were present at the time
+--– each of their interview transcripts mentions the bakery.
