2d93ab55632bf81778435d0d6c35b7946bbbb4ab
Marc <108322460+mkokio@users.noreply.github.com>
2024-03-08 07:15:25 +0000

    Fri, Mar 8, 2024, 4:15 PM +09:00

--- movies/12.sql
+++ movies/12.sql
@@ -5,13 +5,3 @@ WHERE movies.id IN
 AND movies.id IN
 (SELECT movie_id FROM stars WHERE person_id =
   (SELECT id FROM people WHERE name = 'Jennifer Lawrence'));
-WHERE movies.id = stars.movie_id
-AND people.id = stars.person_id
-AND name IN ('Bradley Cooper')
-AND name IN ('Jennifer Lawrence');
-
-list the titles of all movies in which both Bradley Cooper and Jennifer Lawrence starred.
-
-    Your query should output a table with a single column for the title of each movie.
-    You may assume that there is only one person in the database with the name Bradley Cooper.
-    You may assume that there is only one person in the database with the name Jennifer Lawrence.
