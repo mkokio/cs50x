1c5617894944f363fb69ce739a026576468d0494
Marc <108322460+mkokio@users.noreply.github.com>
2024-03-08 05:49:10 +0000

    Fri, Mar 8, 2024, 2:49 PM +09:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,5 +1,1 @@
-
-List the names of all people who starred in Toy Story.
-
-    Your query should output a table with a single column for the name of each person.
-    You may assume that there is only one movie in the database with the title Toy Story.
+SELECT name FROM people, stars, movies WHERE people.id = stars.person_id AND stars.movie_id = movies.id AND title = "Toy Story";
