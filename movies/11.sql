2b48fe6664ee82c0ac484acc20461cff672bfc60
Marc <108322460+mkokio@users.noreply.github.com>
2024-03-08 06:22:42 +0000

    Fri, Mar 8, 2024, 3:22 PM +09:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,7 +1,1 @@
-SELECT title, rating FROM movies, stars, people, ratings WHERE movies.id = stars.movie_id AND people.id = stars.person_id AND people.name = 'Chadwick Boseman' ORDER BY ratings.rating DESC LIMIT 5;
-
-list the titles of the five highest rated movies (in order) that Chadwick Boseman starred in,
-starting with the highest rated.
-
-    Your query should output a table with a single column for the title of each movie.
-    You may assume that there is only one person in the database with the name Chadwick Boseman.
+SELECT title FROM movies, stars, people, ratings WHERE movies.id = stars.movie_id AND people.id = stars.person_id AND people.name = 'Chadwick Boseman' ORDER BY ratings.rating DESC LIMIT 5;
