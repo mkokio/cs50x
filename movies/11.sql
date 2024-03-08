de4f15b376c50f6270984ca830f0bf516c4e2d0c
Marc <108322460+mkokio@users.noreply.github.com>
2024-03-08 06:25:48 +0000

    Fri, Mar 8, 2024, 3:25 PM +09:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,4 +1,5 @@
 SELECT title FROM movies, stars, people, ratings
 WHERE movies.id = stars.movie_id
 AND people.id = stars.person_id
+AND movies.id = ratings.movie_id
 AND people.name = 'Chadwick Boseman' ORDER BY ratings.rating DESC LIMIT 5;
