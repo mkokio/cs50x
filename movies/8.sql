a1e91974d2a0d4ad5b016518a5fef929e99c2554
Marc <108322460+mkokio@users.noreply.github.com>
2024-03-08 05:42:50 +0000

    Fri, Mar 8, 2024, 2:42 PM +09:00

---
+++ movies/8.sql
@@ -0,0 +1,5 @@
+
+List the names of all people who starred in Toy Story.
+
+    Your query should output a table with a single column for the name of each person.
+    You may assume that there is only one movie in the database with the title Toy Story.
